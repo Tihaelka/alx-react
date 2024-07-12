02dad1da50082c3a61b7461f5705ce20a738f340
Tihalka <fkassimi50@gmail.com>
2024-07-12 03:29:10 -0700

    task 3

---
+++ 0x01-react_intro/task_3/dashboards/src/utils.js
@@ -0,0 +1,38 @@
+
+//import React from 'react';
+rt function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  } else {
+    return 'Holberton School main dashboard';
+  }
+}
+
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
+
+
+// export const getFullYear = () => {
+//   return new Date().getFullYear();
+// };
+
+// export const getFooterCopy = (isIndex) => {
+//   return `© ${getFullYear()} Holberton School main dashboard`;
+// };
+
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return 'Holberton School';
+  } else {
+    return 'Holberton School main dashboard';
+  }
+}
